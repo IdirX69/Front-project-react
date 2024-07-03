742edc3c498c4b6099c58f47966224d6cc7636af
IdirX69 <adil.idirr@gmail.com>
2024-07-03 14:56:25 +0200

    login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Register />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Login.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { authenticateUser } from "./session";
+
+const Login = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:5000/auth/login", {
+      method: "POST",
+      body: JSON.stringify(formData),
+      headers: { "Content-type": "application/json" },
+    });
+
+    const { access_token } = await response.json();
+
+    return authenticateUser({ userToken: access_token });
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  return (
+    <form method="POST" onSubmit={handleSubmit}>
+      <input
+        type="email"
+        name="email"
+        value={formData.email}
+        onChange={handleChange}
+        required
+        placeholder="Votre email"
+      />
+      <input
+        type="password"
+        name="password"
+        value={formData.password}
+        onChange={handleChange}
+        required
+        placeholder="Mot de passe"
+      />
+
+      <button type="submit">Se Connecter</button>
+    </form>
+  );
+};
+
+export default Login;
