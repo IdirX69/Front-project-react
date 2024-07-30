ee15f4a58766712e68acf9d65fb18ee88fe72ac1
IdirX69 <adil.idirr@gmail.com>
2024-07-30 13:34:29 +0200

    css produc card / list

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -3,11 +3,13 @@ import ProductCard from "./ProductCard";
 
 const ProductList = ({ products, title }) => {
   return (
-    <div className="product-list-container">
+    <div className="products-list-container">
       <h2>{title}</h2>
-      {products.map((product) => (
-        <ProductCard product={product} />
-      ))}
+      <div>
+        {products.map((product) => (
+          <ProductCard product={product} key={product.id} />
+        ))}
+      </div>
     </div>
   );
 };
