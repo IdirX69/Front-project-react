4b5c15be49231a0f3eb2f8fd83f52c630483c122
IdirX69 <adil.idirr@gmail.com>
2024-09-06 17:52:46 +0200

    all categories page

--- src/pages/AllCategories.tsx
+++ src/pages/AllCategories.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { CategoryType } from "../types/types";
+import Navigation from "../components/Navigation";
 
 const AllCategories = () => {
-  return <div></div>;
+  const apiKey = import.meta.env.VITE_API_KEY;
+  const [categories, setCategories] = useState<CategoryType[]>([]);
+  const fetchCategories = async () => {
+    const response = await fetch(`${apiKey}/categories`);
+    const data = await response.json();
+
+    setCategories(data);
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  console.log(categories);
+
+  return (
+    <div>
+      <Navigation />
+      <h2>Chose your catergory</h2>
+      {categories.map((category) => (
+        <span>{category.name}</span>
+      ))}
+    </div>
+  );
 };
 
 export default AllCategories;
