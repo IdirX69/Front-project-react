742b1abfd49be2a9cb2b4fd7c5737472eab193d9
IdirX69 <adil.idirr@gmail.com>
2024-09-09 13:43:07 +0200

    filter products by category

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,12 +1,60 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
+import { ProductType } from "../types/types";
 
-const ProductList = ({ products, title }) => {
+const ProductList = ({
+  products,
+  title,
+}: {
+  products: ProductType[];
+  title: string;
+}) => {
+  const apiKey = import.meta.env.VITE_API_KEY;
+  const [categories, setCategories] = useState<{ id: string; name: string }[]>(
+    []
+  );
+  const [selectedCategory, setSelectedCategory] = useState<string>("All");
+
+  const fetchCategories = async () => {
+    const response = await fetch(`${apiKey}/categories`);
+    const data = await response.json();
+    setCategories(data);
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  const filteredProducts =
+    selectedCategory === "All"
+      ? products
+      : products.filter(
+          (product) => product.category.name === selectedCategory
+        );
+  console.log(products);
+
   return (
     <div className="products-list-container">
       <h2>{title}</h2>
+
+      <div className="category-filter">
+        <label htmlFor="category-select">Filter by category:</label>
+        <select
+          id="category-select"
+          value={selectedCategory}
+          onChange={(e) => setSelectedCategory(e.target.value)}
+        >
+          <option value="All">All categories</option>
+          {categories.map((category) => (
+            <option key={category.id} value={category.name}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
       <div className="products-list">
-        {products.map((product) => (
+        {filteredProducts.map((product) => (
           <ProductCard product={product} key={product.id} />
         ))}
       </div>
