7f5c3f1f09bceaa1ef80ab4be7ba556e9ec66b47
IdirX69 <adil.idirr@gmail.com>
2024-10-18 19:33:02 +0200

    fix

--- src/components/EmptyCart.tsx
+++ src/components/EmptyCart.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const EmptyCart = () => {
   return (
     <div className="cart-empty-container">
       <h2>Your cart is empty</h2>
+      <NavLink to="/products/all" className="link-admin">
+        <button className="btn-2">Explore Products</button>
+      </NavLink>
     </div>
   );
 };
--- src/components/EmptyOrders.tsx
+++ src/components/EmptyOrders.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const EmptyOrders = () => {
   return (
     <div className="empty-orders-container">
       <span>📦</span> <p>You currently have no orders.</p>
-      <button className="btn-2">Explore Products</button>
+      <NavLink to="/products/all" className="link-admin">
+        <button className="btn-2">Explore Products</button>
+      </NavLink>
     </div>
   );
 };
